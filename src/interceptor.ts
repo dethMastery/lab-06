091a60c81f089b093b344a0f1fd6469138554a75
dethMastery <me@suphakit.net>
2023-04-12 13:30:58 +0700

    chore: added env to interceptor

--- src/interceptor.ts
+++ src/interceptor.ts
@@ -1,8 +1,12 @@
 import axios from "axios"
+import env from "dotenv"
+
+env.config()
+let envData = process.env
 
 axios.interceptors.request.use(async (config: any)=> {
   config.headers = { 
-    'Authorization': '',
+    'Authorization': envData.AUTH_KEY,
     'Content-Type': 'application/x-www-form-urlencoded'
   }
   config.url = 'https://notify-api.line.me/api/notify' + config.url
