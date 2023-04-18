681af230081006b7eba42ee7c59bea39bedd27f4
dethMastery <me@suphakit.net>
2023-04-18 14:40:02 +0700

    feat: add instruction

---
+++ app.ts
@@ -0,0 +1,5 @@
+console.log('Oh! There is nothing Here :(');
+console.log('');
+console.log("Try to run <`package manager` run dev:github> or <`package manager` run dev:line> (don't forget to add auth token first!)");
+console.log('replace `package manager` with your favorite package manager like `yarn`, `pnpm` or `npm`');
+console.log('');
