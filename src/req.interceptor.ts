5a8a59fbf12da1675b1ef583d4a84dccf582de9c
dethMastery <me@suphakit.net>
2023-04-18 14:09:28 +0700

    chore: rename

--- src/req.interceptor.ts
+++ src/req.interceptor.ts
@@ -4,13 +4,14 @@ import env from "dotenv"
 env.config()
 let envData = process.env
 
-axios.interceptors.request.use(async (config: any)=> {
+axios.create().interceptors.request.use(async (config: any)=> {
   config.headers = { 
     'Authorization': envData.AUTH_KEY,
     'Content-Type': 'application/x-www-form-urlencoded'
   }
   config.url = 'https://notify-api.line.me/api/notify' + config.url
+
   return config;
-  
 });
-export const httpClient = axios
+
+export const reqClient = axios
